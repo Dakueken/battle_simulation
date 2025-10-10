c5233f8d567a1c08b8ca83945529f2e71e8dea88
David Kist <dakueken@gmail.com>
2025-10-10 11:32:07 +0200

    Umbenennen von den Screens, vereinfachte Testung von CharacterScreen

---
+++ lib/battle/battle_screen.dart
@@ -0,0 +1,19 @@
+/*
+Zeitleiste
+ListView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: entries.length,
+        itemBuilder: (context, index) {
+          return Container(
+            height: 60,
+            color: Colors.white,
+            child: Row(
+              children: [
+                Icon(Icons.person),
+                Text("Character ${entries[index]}"),
+              ],
+            ),
+          );
+        },
+      ),
+      */
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:battle_simulation/battle_screen.dart/character_screen.dart';
 import 'package:battle_simulation/shared/theme.dart';
-import 'package:battle_simulation/start/mainscreen.dart';
+import 'package:battle_simulation/start/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -14,6 +15,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MainScreen(), theme: AppTheme.lightTheme);
+    return MaterialApp(home: CharacterScreen(), theme: AppTheme.lightTheme);
   }
 }
